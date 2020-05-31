fe08daef375378c40b2095ad6bca1e7a833ada8b
Stella Vilar <vilar.stella@gmail.com>
2020-05-31 23:26:41 +0200

    Reactv16 mailto installed to manage mail link

--- react-cv/src/components/Contact/index.js
+++ react-cv/src/components/Contact/index.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import { Container } from 'semantic-ui-react';
+import Mailto from 'reactv16-mailto';
 
 import linkedin from '../../img/linkedin.png';
 import githubLogo from '../../img/GitHub-Mark-32px.png';
@@ -17,7 +18,10 @@ class Contact extends Component {
         <a href="https://github.com/Stellavilar">
             <img src={githubLogo} alt="github logo" className="github-logo"/>
         </a>
-        <p>vilar.stella@gmail.com</p>
+        <Mailto email="vilar.stella@gmail.com" obfuscate={true} >
+        vilar.stella@gmail.com
+    </Mailto >
+        
       </Container>
       <footer>
        &copy; 2020 - Tous droits réservés
