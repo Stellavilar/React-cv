33c025b7d0cbedf35dfd8454861178084c7aac1c
Stella Vilar <vilar.stella@gmail.com>
2020-05-31 23:18:58 +0200

    Contact component done

--- react-cv/src/components/Contact/index.js
+++ react-cv/src/components/Contact/index.js
@@ -1,5 +1,8 @@
 import React, {Component} from 'react';
+import { Container } from 'semantic-ui-react';
 
+import linkedin from '../../img/linkedin.png';
+import githubLogo from '../../img/GitHub-Mark-32px.png';
 
 class Contact extends Component {
   
@@ -7,7 +10,18 @@ class Contact extends Component {
 
     return (
     <div className="contact" id="contact">
-      
+      <Container>
+        <a href="https://www.linkedin.com/in/stella-vilar-dev/">
+            <img src={linkedin} alt="linkedin logo" className="linkedin-logo"/>
+        </a>
+        <a href="https://github.com/Stellavilar">
+            <img src={githubLogo} alt="github logo" className="github-logo"/>
+        </a>
+        <p>vilar.stella@gmail.com</p>
+      </Container>
+      <footer>
+       &copy; 2020 - Tous droits réservés
+      </footer>
     </div>
     );
   }
