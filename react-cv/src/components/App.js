ffa25fa4fa9ac672eb19023b85cec02fcd56c3e5
Stella Vilar <vilar.stella@gmail.com>
2020-06-15 13:10:48 +0200

    Upgrade websocket-extensions

--- react-cv/src/components/App.js
+++ react-cv/src/components/App.js
@@ -1,12 +1,12 @@
 import React, {Component} from 'react';
 
-
 import Head from './Head';
 import Banner from './Banner';
 import Skills from './Skills';
 import Projects from './Projects';
 import Exp from './Exp';
 import Contact from './Contact';
+
 
 class App extends Component {
   
