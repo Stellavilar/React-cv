97fce9d771f83530f1f12d5f4fa6da4882a29fa8
Stella Vilar <vilar.stella@gmail.com>
2020-06-17 11:50:17 +0200

    Pdf file is ok to be download with new title

--- react-cv/src/components/Banner/index.js
+++ react-cv/src/components/Banner/index.js
@@ -1,16 +1,17 @@
 import React, {Component} from 'react';
+
 import pdf from '../../img/CV Vilar Stella.pdf';
 
 class Banner extends Component {
   
   render() {
-      
+  
     return (
     <div className="banner" id="banner">
       <p className="banner-text">Développeuse Web</p>
       <p className="banner-text">Fullstack Javascript</p>
       <p className="banner-text">React JS</p>
-      <a href={pdf}  target="blank"><button>Voir CV</button></a>
+      <a  href={pdf} download="CV Vilar Stella"><button>Télécharger CV</button></a>
     </div>
     );
   };
