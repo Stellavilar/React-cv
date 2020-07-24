1349055b211b97f3a43a03de45ebb04a48582ed0
Stella Vilar <vilar.stella@gmail.com>
2020-07-24 19:46:11 +0200

    Logo pictures imported on skills component

--- react-cv/src/components/Skills/index.js
+++ react-cv/src/components/Skills/index.js
@@ -1,5 +1,17 @@
 import React, {Component} from 'react';
-import { Segment, Header } from 'semantic-ui-react';
+import { Card, Header, Segment } from 'semantic-ui-react';
+
+//Logo pictures imported there:
+import jslogo from '../../img/jslogo.png';
+import htmlogo from '../../img/html.png';
+import csslogo from '../../img/css3.png';
+import reactlogo from '../../img/react.png';
+import bootstrap from '../../img/bootstrap.png';
+import node from '../../img/node.png';
+import express from '../../img/expressjs.png';
+import mongo from '../../img/mongodb.png';
+import postgres from '../../img/postgres.png';
+import sass from '../../img/sass.png';
 
 class Skills extends Component {
   
@@ -9,31 +21,40 @@ class Skills extends Component {
     <div className="skills" id="skills">
         <Header as="h2" className="skills-header">COMPÉTENCES TECHNIQUES</Header>
         <Segment>
-          <Header as="h3">LANGAGE: </Header>
-          <ul>
-            <li>Javascript</li>
-          </ul>
-        </Segment>
-        <Segment>
-          <Header as="h3">FRONT: </Header>
-          <ul>
-            <li>Javascript Vanilla</li>
-            <li>React JS</li>
-            <li>Redux</li>
-            <li>Babel</li>
-            <li>Webpack</li>
-            <li>HTML5</li>
-            <li>CSS3</li>
-          </ul>
-        </Segment>
-        <Segment>
-          <Header as="h3">BACK: </Header>
-          <ul>
-            <li>ExpressJS</li>
-            <li>NodeJS</li>
-            <li>PostgreSQL</li>
-            <li>MongoDB</li>
-          </ul>
+          <Card.Group >
+            <Card>
+              <Card.Content>
+                <img className='logo' src={jslogo} alt='javascript logo'/>
+              </Card.Content>
+              <Card.Content>
+                <img className='logo' src={htmlogo} alt='html logo'/>
+              </Card.Content>
+              <Card.Content>
+                <img className='logo' src={csslogo} alt='css logo'/>
+              </Card.Content>
+              <Card.Content>
+                <img className='logo' src={reactlogo} alt='react logo' />
+              </Card.Content>
+              <Card.Content>
+                <img className='logo' src={bootstrap} alt='bootstrap logo' />
+              </Card.Content>
+              <Card.Content>
+                <img className='logo' src={node} alt='node logo' />
+              </Card.Content>
+              <Card.Content>
+                <img className='logo' src={express} alt='express js logo' />
+              </Card.Content>
+              <Card.Content>
+                <img className='logo' src={mongo} alt='mongo db logo' />
+              </Card.Content>
+              <Card.Content>
+                <img className='logo' src={postgres} alt='postresql logo' />
+              </Card.Content>
+              <Card.Content>
+                <img className='logo' src={sass} alt='sass logo' />
+              </Card.Content>
+            </Card>
+            </Card.Group>
         </Segment>
     </div>
     );
