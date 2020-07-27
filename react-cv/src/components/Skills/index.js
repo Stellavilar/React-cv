b1fdbc1624246e3aef752da64488d5fae2007436
Stella Vilar <vilar.stella@gmail.com>
2020-07-27 23:43:14 +0200

    Progress Bar installed

--- react-cv/src/components/Skills/index.js
+++ react-cv/src/components/Skills/index.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { Card, Header, Image } from 'semantic-ui-react';
+import { Card, Header, Image, Progress } from 'semantic-ui-react';
 
 //Logo pictures imported there:
 import jslogo from '../../img/jslogo.png';
@@ -23,33 +23,70 @@ class Skills extends Component {
           <Card.Group >
             <Card>
               <Image className='logo' src={jslogo} alt='javascript logo' wrapped ui={false}/>
+              <Card.Content>
+                <p>Javascript Vanilla</p>
+                <Progress percent={75} color='grey' />
+              </Card.Content>
             </Card>
             <Card>
               <Image className='logo' src={htmlogo} alt='html logo'/>
+              <Card.Content>
+                <p>HTML 5</p>
+                <Progress percent={85} color='grey' />
+              </Card.Content>
             </Card>
             <Card>
               <Image className='logo' src={csslogo} alt='css logo'/>
+              <Card.Content>
+                <p>CSS 3</p>
+                <Progress percent={85} color='grey' />
+              </Card.Content>
             </Card>
             <Card >
               <Image className='logo' src={sass} alt='sass logo' />
+              <Card.Content>
+                <p>SASS</p>
+                <Progress percent={50} color='grey' />
+              </Card.Content>
             </Card>
             <Card className='react'>
               <Image className='logo' src={reactlogo} alt='react logo' />
+              <Card.Content>
+                <p>React / Redux</p>
+                <Progress percent={60} color='grey' />
+              </Card.Content>
             </Card>
-            <Card className='bootstrap'>
+            <Card >
               <Image className='logo' src={bootstrap} alt='bootstrap logo' />
+              <Card.Content>
+                <Progress percent={50} color='grey' />
+              </Card.Content>
             </Card>
             <Card>
               <Image className='logo' src={postgres} alt='postresql logo' />
+              <Card.Content>
+                <p>PostgreSQL</p>
+                <Progress percent={83} color='grey' />
+              </Card.Content>
             </Card>
             <Card className='node'>
               <Image className='logo' src={node} alt='node logo' />
+              <Card.Content>
+                <p>Node JS</p>
+                <Progress percent={83} color='grey' />
+              </Card.Content>
             </Card>
             <Card >
               <Image className='logo' src={express} alt='express js logo' />
+              <Card.Content>
+                <Progress percent={90} color='grey' />
+              </Card.Content>
             </Card>
             <Card className='mongo'>
               <Image className='logo' src={mongo} alt='mongo db logo' />
+              <Card.Content>
+                <Progress percent={40} color='grey' />
+              </Card.Content>
             </Card>
           </Card.Group>
     </div>
