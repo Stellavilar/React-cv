b1da88e4b88de46628765c9dc4eb304981e211b6
Stella Vilar <vilar.stella@gmail.com>
2020-11-16 15:31:57 +0100

    Update Projects and Skills

--- react-cv/src/components/Projects/index.js
+++ react-cv/src/components/Projects/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import { Card, Header, Container } from 'semantic-ui-react';
-import YoutubeSAV from '../YoutubeSAV';
+import { Card, Header, Container, Image, Icon } from 'semantic-ui-react';
 import YoutubePokedex from '../YoutubePokedex';
 
-import githubLogo from '../../img/GitHub-Mark-32px.png';
 import funnyQuiz from '../../img/FunnyQuiz.png';
+import savConnect from '../../img/sav-connect.png';
 
 
 const Projects = () => {
@@ -15,14 +14,61 @@ const Projects = () => {
       <Container text>
         <p>Voici des projets développés seule et également dans le cadre de ma formation de Développeur et Web et Web mobile</p>
       </Container>
-      <Card.Group >
+      <div className='applis'>
         <Card>
+          <Image src={funnyQuiz} wrapped ui={false} as='a' href='http://ec2-54-91-42-56.compute-1.amazonaws.com/' target='blank' />
+        <Card.Content>
+          <Card.Header>Funny Quiz</Card.Header>
+          <Card.Meta>
+            <span className='description'>Quizz interactif pour tous les âges</span>
+          </Card.Meta>
+          <Card.Description>
+            Funny Quizz est une application que j'ai développé seule, afin de parfaire mes compétences en SQL et ReactJS. 
+            Ce jeu regroupe des questions et réponses que j'ai récupéré via des fichiers SQL disponibles sur le site OpenQuizzDb.com.
+            J'ai utilisé une base de donnée hebergée sous Postgres et le Front est codé 100% en React JS.
+          </Card.Description>
+        </Card.Content>
+        <Card.Content extra>
+          <a href="https://github.com/Stellavilar/FunnyQuiz" target="blank">
+            <Icon name='github' />
+            GitHub Repository
+          </a>
+        </Card.Content>
+        </Card>
+        <Card>
+          <Image src={savConnect} wrapped ui={false} as='a' href='http://ec2-18-208-219-38.compute-1.amazonaws.com' target='blank' />
+        <Card.Content>
+          <Card.Header>Sav-connect</Card.Header>
+          <Card.Meta>
+            <span className='description'>Plateforme de gestion de service après-vente pour un atelier de réparation</span>
+          </Card.Meta>
+          <Card.Description>
+              Sav-connect permet de gérer la prise en charge d'appareils en panne et de créer, répertorier, suivre et mettre à jour les interventions, les devis, etc.<br/>
+              C'est une application développée au sein d'un groupe de 4 développeurs. Mon rôle fut Lead Dev Front.<br/>
+              Le Front est codé en React et le Back via Node, avec une base de donnée Postgres.<br/>
+              <span style={{color: "palevioletred"}}>Identifiants de connexion:</span> admin@admin.fr / mot de passe: Admin1234?
+          </Card.Description>
+        </Card.Content>
+        <Card.Content extra>
+          <a href="https://github.com/Stellavilar/sav-connect" target="blank">
+            <Icon name='github' />
+            GitHub Repository
+          </a>
+        </Card.Content>
+        </Card>
+      </div>
+      {/* <Card.Group >
+      <Card>
           <Card.Content>
-            <YoutubeSAV videoId="W2_JOKIKyF0" />
-            <Header as='h3'>SAV CONNECT</Header>
-            <p>Développement d’une application de gestion de service après-vente à destination d’un professionnel.</p>
-            <p>Front: ReactJs</p>
-            <p>Back: PostgreSQL, ExpressJS, NodeJs</p>
+            <a href="ec2-18-208-219-38.compute-1.amazonaws.com" target="blank">
+              <img className="funny-quiz" src={funnyQuiz} alt="Funny Quiz" />
+            </a>
+            <Header as='h3'>Sav-connect</Header>
+            <p>Dans le cadre de ma formation, j'ai développé une plateforme utile pour les SAV d'entreprise.</p> <br/>
+            <p>Sav-connect permet de gérer la prise en charge d'appareils en panne et de créer, répertorier, suivre et mettre à jour les interventions, les devis, etc.</p><br/>
+            <p>C'est également une application nécessaire au bon fonctionnement interne d'un atelier de réparation</p><br/>
+            <p>Le Front est codé en React et le Back via Node, avec une base de donnée SQL de Postgres.</p><br/>
+            <p>Identifiants de connexion: admin@admin.fr / mot de passe: Admin1234?</p>
             <div className="git-place">
               <img className="github-logo" src={githubLogo} alt="github logo" />
               <a className="github-link" href="https://github.com/Stellavilar/sav-connect" target="blank">Github Repository</a>
@@ -31,7 +77,7 @@ const Projects = () => {
         </Card>
         <Card>
           <Card.Content>
-            <a href="https://google.fr" target="blank">
+            <a href="ec2-54-91-42-56.compute-1.amazonaws.com" target="blank">
               <img className="funny-quiz" src={funnyQuiz} alt="Funny Quiz" />
             </a>
             <Header as='h3'>Funny Quiz</Header>
@@ -55,7 +101,7 @@ const Projects = () => {
             </div>
           </Card.Content>
         </Card>
-      </Card.Group>
+      </Card.Group> */}
     </div>
   );
 };
--- react-cv/src/components/Skills/index.js
+++ react-cv/src/components/Skills/index.js
@@ -1,17 +1,6 @@
 import React, {Component} from 'react';
-import { Card, Header, Image, Progress } from 'semantic-ui-react';
+import { Header, List } from 'semantic-ui-react';
 
-//Logo pictures imported there:
-import jslogo from '../../img/jslogo.png';
-import htmlogo from '../../img/html.png';
-import csslogo from '../../img/css3.png';
-import reactlogo from '../../img/react.svg';
-import bootstrap from '../../img/bootstrap.png';
-import node from '../../img/node.png';
-import express from '../../img/expressjs.png';
-import mongo from '../../img/mongodb.png';
-import postgres from '../../img/postgres.png';
-import sass from '../../img/sass.png';
 
 class Skills extends Component {
   
@@ -20,85 +9,48 @@ class Skills extends Component {
     return (
     <div className="skills" id="skills">
         <Header as="h2" className="skills-header">COMPÉTENCES TECHNIQUES</Header>
-        <ul>
-          <li>Méthode AGILE (Scrum, Kanban, Github)</li>
-          <li>Réalisation de wireframes et maquettes (diagrams.net, figma.com)</li>
-          <li>Réalisation d'interfaces web statiques (Javascript, React)</li>
-          <li>Développement d'interfaces utilisateur Web dynamiques (Javascript, React, Express JS, EJS)</li>
-          <li>Réalisation d'interfaces utilisateur avec contenu e-commerce (Prestashop, Shopify)</li>
-          <li>Création et branchement de base de données (Express JS, Node JS, PostgreSQL, Mongodb)</li>
-          <li>Utilisation de Websockets (réalisation d'un tchat)</li>
-          <li>Développement Back-end (sqitch, Happi, PSQL, API, AJAX)</li>
-        </ul>
-          <Card.Group >
-            <Card>
-              <Image className='logo' src={jslogo} alt='javascript logo' wrapped ui={false}/>
-              <Card.Content>
-                <p>Javascript Vanilla</p>
-                <Progress percent={75} color='grey' />
-              </Card.Content>
-            </Card>
-            <Card>
-              <Image className='logo' src={htmlogo} alt='html logo'/>
-              <Card.Content>
-                <p>HTML 5</p>
-                <Progress percent={85} color='grey' />
-              </Card.Content>
-            </Card>
-            <Card>
-              <Image className='logo' src={csslogo} alt='css logo'/>
-              <Card.Content>
-                <p>CSS 3</p>
-                <Progress percent={85} color='grey' />
-              </Card.Content>
-            </Card>
-            <Card >
-              <Image className='logo' src={sass} alt='sass logo' />
-              <Card.Content>
-                <p>SASS</p>
-                <Progress percent={50} color='grey' />
-              </Card.Content>
-            </Card>
-            <Card className='react'>
-              <Image className='logo' src={reactlogo} alt='react logo' />
-              <Card.Content>
-                <p>React / Redux</p>
-                <Progress percent={60} color='grey' />
-              </Card.Content>
-            </Card>
-            <Card >
-              <Image className='logo' src={bootstrap} alt='bootstrap logo' />
-              <Card.Content>
-                <Progress percent={50} color='grey' />
-              </Card.Content>
-            </Card>
-            <Card>
-              <Image className='logo' src={postgres} alt='postresql logo' />
-              <Card.Content>
-                <p>PostgreSQL</p>
-                <Progress percent={83} color='grey' />
-              </Card.Content>
-            </Card>
-            <Card className='node'>
-              <Image className='logo' src={node} alt='node logo' />
-              <Card.Content>
-                <p>Node JS</p>
-                <Progress percent={83} color='grey' />
-              </Card.Content>
-            </Card>
-            <Card >
-              <Image className='logo' src={express} alt='express js logo' />
-              <Card.Content>
-                <Progress percent={90} color='grey' />
-              </Card.Content>
-            </Card>
-            <Card className='mongo'>
-              <Image className='logo' src={mongo} alt='mongo db logo' />
-              <Card.Content>
-                <Progress percent={40} color='grey' />
-              </Card.Content>
-            </Card>
-          </Card.Group>
+        <List>
+          <List.Item>
+            <List.Icon name='check square outline' />
+            <List.Content>Méthode AGILE (Scrum, Kanban, Github)</List.Content>
+          </List.Item>
+          <List.Item>
+            <List.Icon name='check square outline' />
+            <List.Content>Réalisation de wireframes et maquettes (diagrams.net, figma.com)</List.Content>
+          </List.Item>
+          <List.Item>
+            <List.Icon name='check square outline' />
+            <List.Content>Réalisation d'interfaces web statiques (Javascript, React)</List.Content>
+          </List.Item>
+          <List.Item>
+            <List.Icon name='check square outline' />
+            <List.Content>Développement d'interfaces utilisateur Web dynamiques (Javascript, React, Express JS, EJS)</List.Content>
+          </List.Item>
+          <List.Item>
+            <List.Icon name='check square outline' />
+            <List.Content>Réalisation d'interfaces utilisateur avec contenu e-commerce (Prestashop, Shopify)</List.Content>
+          </List.Item>
+          <List.Item>
+            <List.Icon name='check square outline' />
+            <List.Content>Création et branchement de base de données (Express JS, Node JS, PostgreSQL, Mongodb)</List.Content>
+          </List.Item>
+          <List.Item>
+            <List.Icon name='check square outline' />
+            <List.Content>Utilisation de Websockets (réalisation d'un tchat)</List.Content>
+          </List.Item>
+          <List.Item>
+            <List.Icon name='check square outline' />
+            <List.Content>Développement Back-end (sqitch, Happi, PSQL, API, AJAX)</List.Content>
+          </List.Item>
+          <List.Item>
+            <List.Icon name='download' />
+            <List.Content>PHP</List.Content>
+          </List.Item>
+          <List.Item>
+            <List.Icon name='download' />
+            <List.Content>JQuery</List.Content>
+          </List.Item>
+        </List>
     </div>
     );
   }
